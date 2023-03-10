330c53711592de38219d4ab0776b199f992c4da6
Taras <102531278+Taras85@users.noreply.github.com>
2023-03-10 12:59:32 +0200

    buton activ

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const butonChangeColor = document.querySelector('[data-start]');
+console.log(butonChangeColor);
+const butonStopColor = document.querySelector('[data-stop]');
+const bodyChangeColor = document.querySelector('body');
+
+butonChangeColor.addEventListener('click', () => {
+  bodyChangeColor.style.backgroundColor = getRandomHexColor();
+  butonChangeColor.setAttribute('disabled', false);
+  butonStopColor.removeAttribute('disabled');
+});
+
+butonStopColor.addEventListener('click', () => {
+  butonStopColor.setAttribute('disabled', false);
+  butonChangeColor.removeAttribute('disabled');
+});
